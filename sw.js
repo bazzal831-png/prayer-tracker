4dfd8537a64d9fbc331a3118d06a39e05562c3de
bazzal831-png <bazzal831@gmail.com>
2026-06-19 15:07:07 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'prayer-tracker-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.png'
+];
+
+// تنصيب الـ Service Worker وحفظ الملفات في الكاش
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// جلب الملفات من الكاش أولاً (للسرعة والعمل بدون إنترنت)
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
